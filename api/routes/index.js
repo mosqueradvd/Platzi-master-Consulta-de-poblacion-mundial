ab662864b588eaa3e51d5a2304b89d435a39ccd1
efrenmartinez <developer.emartinez@gmail.com>
2020-06-09 20:10:32 -0500

    Se elimino hapi-mongo

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,8 @@
 const continent = require("./continent");
 const country = require("./country");
 const error = require("./error");
+const topCountry = require("./topCountry");
 
-let routes = [...continent, ...country, ...error];
+let routes = [...continent, ...country, ...error, ...topCountry];
 
 module.exports = routes;
