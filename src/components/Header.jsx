4d7474d325d8c575027e12aa6cd52561d42047f2
Lennin666 <soporte@allnexus.com.co>
2020-06-25 18:16:06 -0500

    agregando estilos al formulario de contacto y al menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,74 @@
 import React from 'react';
 import '../assets/Globals.scss';
 import '../assets/styles/components/Header.scss';
-{/* import logo from '../assets/static/react.svg'; */ }
+import {
+  AiFillSignal,
+  AiFillTrophy,
+  AiOutlineDotChart,
+  AiFillEnvironment,
+  AiFillMail,
+  AiOutlineHome,
+} from 'react-icons/ai';
 
-const Header = () => (
-  <header className='header'>
-    {/* <h1 className='header__title'>
+{ /* import logo from '../assets/static/react.svg'; */ }
+
+const Header = () => {
+  return (
+    <header className='header'>
+      {/* <h1 className='header__title'>
       Menu
     </h1> */}
-    {/* <h1>World Population</h1> */}
-    <nav className='header__menuCSS3'>
-      <ul>
-        <li>
-          <a href='#popul'>Menu</a>
-          <ul>
-            <li>
-              <a href='#statistics'>Statistics</a>
-            </li>
-            <li>
-              <a href='#topten'>Top 10</a>
-            </li>
-            <li>
-              <a href='#wstats'>World Statistics</a>
-            </li>
-            <li>
-              <a href='#map'>Map</a>
-            </li>
-            <li>
-              <a href='#contact'>Contact</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+      {/* <h1>World Population</h1> */}
+      <nav className='header__menuCSS3'>
+        <ul>
+          <li>
+            <a href='#popul'>
+              <AiOutlineHome />
+              {' '}
+              Menu
+            </a>
+            <ul>
+              <li>
+                <a href='#statistics'>
+                  <AiFillSignal />
+                  {' '}
+                  Statistics
+                </a>
+              </li>
+              <li>
+                <a href='#topten'>
+                  <AiFillTrophy />
+                  {' '}
+                  Top 10
+                </a>
+              </li>
+              <li>
+                <a href='#wstats'>
+                  <AiOutlineDotChart />
+                  {' '}
+                  World Statistics
+                </a>
+              </li>
+              <li>
+                <a href='#map'>
+                  <AiFillEnvironment />
+                  {' '}
+                  Map
+                </a>
+              </li>
+              <li>
+                <a href='#contact'>
+                  <AiFillMail />
+                  {' '}
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/WorldPopulationRegion.jsx
+++ src/components/WorldPopulationRegion.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "../assets/styles/components/WorldPopulationRegion.scss";
+import React from 'react';
+import '../assets/styles/components/WorldPopulationRegion.scss';
 import {
   BarChart,
   Bar,
@@ -9,25 +9,25 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
-} from "recharts";
+} from 'recharts';
 
 const dataBurned = [
-  { continent: "China", Population: 4000, amt: 2400 },
-  { continent: "India", Population: 3000, amt: 2210 },
-  { continent: "U.S.A", Population: 2000, amt: 2290 },
-  { continent: "Indonesia", Population: 2780, amt: 2000 },
-  { continent: "Pakistan", Population: 1890, amt: 2181 },
-  { continent: "Brasil", Population: 2390, amt: 2500 },
-  { continent: "Nigeria", Population: 3490, amt: 2100 },
-  { continent: "Bangladesh", Population: 3490, amt: 2100 },
-  { continent: "Rusia", Population: 3490, amt: 2100 },
-  { continent: "México", Population: 3490, amt: 2100 },
+  { continent: 'China', Population: 4000, amt: 2400 },
+  { continent: 'India', Population: 3000, amt: 2210 },
+  { continent: 'U.S.A', Population: 2000, amt: 2290 },
+  { continent: 'Indonesia', Population: 2780, amt: 2000 },
+  { continent: 'Pakistan', Population: 1890, amt: 2181 },
+  { continent: 'Brasil', Population: 2390, amt: 2500 },
+  { continent: 'Nigeria', Population: 3490, amt: 2100 },
+  { continent: 'Bangladesh', Population: 3490, amt: 2100 },
+  { continent: 'Rusia', Population: 3490, amt: 2100 },
+  { continent: 'México', Population: 3490, amt: 2100 },
 ];
 
 const WorldPopulationRegion = () => {
   // console.log(dataBurned);
   return (
-    <div className="cambiarNombreDeClase">
+    <div className='cambiarNombreDeClase'>
       <div style={{ width: '100%', height: 500 }}>
         <h1>Chart WorldPopulation by Region</h1>
         <ResponsiveContainer>
@@ -37,15 +37,15 @@ const WorldPopulationRegion = () => {
             data={dataBurned}
             margin={{ top: 50, right: 30, left: 40, bottom: 5 }}
           >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="continent" />
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='continent' />
             <YAxis />
             <Tooltip />
             <Legend />
             <Bar
-              dataKey="Population"
-              fill="#82ca9d"
-              background={{ fill: "#eee" }}
+              dataKey='Population'
+              fill='#82ca9d'
+              background={{ fill: '#eee' }}
               label={{ position: 'top' }}
             />
             {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
